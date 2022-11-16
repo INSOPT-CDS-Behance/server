bd19db15e5aa95ce3f46adf5d1936a00d7d78fb6
hyesuuou <68391767+hyesuuou@users.noreply.github.com>
2022-11-16 18:42:27 +0900

    [Chore] Response Message, Status Code 추가 (#4)

---
+++ cds-server/src/constants/responseMessage.ts
@@ -0,0 +1,12 @@
+const message = {
+    // 200
+    
+    // 400
+    BAD_PATH: '잘못된 경로입니다.',
+    NULL_VALUE: '필요한 값이 없습니다.',
+
+    // 500
+    INTERNAL_SERVER_ERROR: '서버 내부 오류',
+};
+
+export default message;
