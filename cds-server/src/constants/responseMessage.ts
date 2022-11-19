787684790c6dcd0250a4a3441a04663c82f87830
hannah100-coder <pd206@naver.com>
2022-11-20 01:31:43 +0900

    [Chore] constants, interfaces 폴더 추가

--- cds-server/src/constants/responseMessage.ts
+++ cds-server/src/constants/responseMessage.ts
@@ -2,8 +2,10 @@ const message = {
     // 200
     
     // 400
-    BAD_PATH: '잘못된 경로입니다.',
     NULL_VALUE: '필요한 값이 없습니다.',
+    OUT_OF_VALUE: "파라미터 값이 잘못되었습니다.",
+    NOT_FOUND: "잘못된 경로입니다.",
+    BAD_REQUEST: "잘못된 요청입니다.",
 
     // 500
     INTERNAL_SERVER_ERROR: '서버 내부 오류',
