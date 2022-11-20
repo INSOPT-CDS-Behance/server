48a9010e63cbbc97cdaed9a06b3ebc37286d4165
yelin <yelin@kim-yelins-MacBook-Pro.local>
2022-11-21 02:09:55 +0900

    [Feat]: post 좋아요 api 구현 (#9)

--- cds-server/src/controller/likeController.ts
+++ cds-server/src/controller/likeController.ts
@@ -1,8 +1,44 @@
 import { Request, Response } from "express";
 import { likeService } from "../service";
+import { rm, sc } from "../constants";
+import { success, fail } from "../constants/response";
+
+//좋아요 누르기
+const like = async(req: Request, res:Response)=>{
+    const{projectId,userId} = req.body;
+     //userId 일단 현재사용자로 고정, 존재하는 프로젝트인지만 확인
+    if(!projectId){
+        return res.status(sc.BAD_REQUEST).send(fail(sc.BAD_REQUEST,rm.NOT_FOUND))
+    }
+    
+    const isliked = await likeService.isLiked(projectId,userId);
+    if(isliked){
+        return dislike;
+    }
+
+    const likeHistory = await likeService.createLikeHistory(projectId,userId);
+    if(!likeHistory){
+        return res.status(sc.BAD_REQUEST).send(fail(sc.BAD_REQUEST,rm.NOT_FOUND))
+
+    }
+    const likeSum = await likeService.updateLikeSum(projectId);
+    if(!likeSum){
+        return res.status(sc.BAD_REQUEST).send(fail(sc.BAD_REQUEST,rm.NOT_FOUND))
+    }
+
+    return res.status(sc.OK).send(success(sc.OK, rm.POST_LIKE_SUCCESS,));  
+
+}
+
+//좋아요 취소하기
+const dislike = async(req:Request, res:Response)=>{
+
+
+}
+
 
 const likeController = {
-
+    like,
 };
 
 export default likeController;
--- cds-server/src/router/likeRouter.ts
+++ cds-server/src/router/likeRouter.ts
@@ -3,5 +3,8 @@ import { likeController } from "../controller";
 
 const router: Router = Router();
 
+//api/project/like
+router.post("/like",likeController.like);
+//router.delete("/like",likeController.dislike);
 
 export default router;
