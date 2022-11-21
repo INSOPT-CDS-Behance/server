3364dfd584ac67d0cac7b86d59058cf0a6920041
hannah100-coder <pd206@naver.com>
2022-11-21 21:36:16 +0900

    [Fix] addTryCatch (#23)

--- cds-server/src/service/moodboardService.ts
+++ cds-server/src/service/moodboardService.ts
@@ -1,6 +1,9 @@
 import { MoodboardPatchDTO } from './../interfaces/moodboard/MoodboardPatchDTO';
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
+import { rm, sc } from "../constants"
+import { fail } from  "../constants/response"
+import { Response } from "express";
 
 //*  무드보드 전체 조회
 const getAllMoodboard = async () => {
@@ -20,15 +23,21 @@ const getAllMoodboard = async () => {
 }
 
 const updateMoodboard = async (MoodboardPatchDTO: MoodboardPatchDTO) => {
-    const data = await prisma.moodboard.update({
-        where: {
-            id: MoodboardPatchDTO.id
-        },
-        data: {
-            is_public: MoodboardPatchDTO.is_public
-        }
-    });
-    return data;
+    try {
+        const data = await prisma.moodboard.update({
+            where: {
+                id: MoodboardPatchDTO.id
+            },
+            data: {
+                is_public: MoodboardPatchDTO.is_public
+            }
+        });
+        return data;
+    } catch(error) {
+        console.log(error);
+        //throw error;
+        return rm.INTERNAL_SERVER_ERROR
+    }
 }
 
 const moodboardService = {
